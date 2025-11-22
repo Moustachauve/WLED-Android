0c5287620b1787040984de7429a734ec843e3616
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-11-22 14:36:49 -0500

    Change applicationId for Illumidel app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "ca.cgagnier.wlednativeandroid"
+        applicationId = "ca.cgagnier.illumidel"
         minSdk = 24
         targetSdk = 36
         versionCode = 41
