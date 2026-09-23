d8315d11ac35352551a909ebc4bc5b63b411d8b8
Moustachauve <2206577+Moustachauve@users.noreply.github.com>
2026-09-22 22:03:00 -0400

    ci: optimize GitHub Actions cache and lint analysis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,9 @@ android {
     namespace = "ca.cgagnier.wlednativeandroid"
     lint {
         fatal += "MissingTranslation"
+        checkTestSources = false
+        checkDependencies = false
+        abortOnError = true
     }
 }
 
