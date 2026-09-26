307e3e760d4685c622add72cdf9a74e3984f43be
Moustachauve <2206577+Moustachauve@users.noreply.github.com>
2026-09-25 21:42:27 -0400

    test: clean up unused imports and standardize UTF-8 decoding

--- app/src/test/java/ca/cgagnier/wlednativeandroid/repository/migrations/UserPreferencesV0ToV1Test.kt
+++ app/src/test/java/ca/cgagnier/wlednativeandroid/repository/migrations/UserPreferencesV0ToV1Test.kt
@@ -5,7 +5,6 @@ import ca.cgagnier.wlednativeandroid.repository.UserPreferences
 import com.diffplug.selfie.Selfie.expectSelfie
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
